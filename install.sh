fec842cd014baf05b2c2aadb23391976217d0499
Frank Scheiner <frank.scheiner@web.de>
2012-11-02 10:08:52 +0100

    Adapted installer script to changed directory structure.

--- install.sh
+++ install.sh
@@ -3,32 +3,32 @@
 #  install.sh / uninstall.sh - Install or uninstall software
 
 prefixDir="$HOME/opt"
-# nonUserInstall activated? 0 => yes, 1 => no
-nonUserInstall=1
+# nonUserInstall activated? 0 => no, 1 => yes
+userInstall=1
 
 if [[ "$1" != "" ]]; then
 	prefixDir="$1"
-	nonUserInstall=0
+	userInstall=0
 fi
 
 if [[ "$(basename $0)" == "install.sh" ]]; then
 
 	#  first create bin dir in home, if not already existing
-	if [[ $nonUserInstall -eq 1 ]]; then	
+	if [[ $userInstall -eq 1 ]]; then	
 		if [[ ! -e "$HOME/bin" ]]; then
 			mkdir -p "$HOME/bin" &>/dev/null
 		fi
 	fi
 
 	mkdir -p "$prefixDir/tgftp/bin" &>/dev/null
-	mkdir -p "$prefixDir/tgftp/doc" &>/dev/null
-	mkdir -p "$prefixDir/tgftp/man/man1" &>/dev/null
+	mkdir -p "$prefixDir/tgftp/share/doc" &>/dev/null
+	mkdir -p "$prefixDir/tgftp/share/man/man1" &>/dev/null
 
 	#  copy scripts and...
-	cp ./testgftp.sh "$prefixDir/tgftp/bin"
-	cp ./testgftp_log.sh "$prefixDir/tgftp/bin"
+	cp ./bin/testgftp.sh "$prefixDir/tgftp/bin"
+	cp ./bin/testgftp_log.sh "$prefixDir/tgftp/bin"
 	#  ...make links
-	if [[ $nonUserInstall -eq 1 ]]; then
+	if [[ $userInstall -eq 1 ]]; then
 		linkPath="$HOME"
 	else
 		linkPath="$prefixDir/tgftp"
@@ -37,16 +37,18 @@ if [[ "$(basename $0)" == "install.sh" ]]; then
 	ln -s "$prefixDir/tgftp/bin/testgftp_log.sh" "$linkPath/bin/tgftp_log"
 
 	#  copy README and manpages
-	cp ./README "$prefixDir/tgftp/doc"
-	cp ./tgftp.1.pdf ./tgftp_log.1.pdf "$prefixDir/tgftp/doc"
-	cp ./COPYING "$prefixDir/tgftp/doc"
-	cp ./tgftp.1 "$prefixDir/tgftp/man/man1"
-	cp ./tgftp_log.1 "$prefixDir/tgftp/man/man1"
+	cp ./share/doc/README "$prefixDir/tgftp/share/doc"
+	cp ./share/doc/tgftp.1.pdf ./share/doc/tgftp_log.1.pdf "$prefixDir/tgftp/share/doc"
+	cp ./COPYING "$prefixDir/tgftp/share/doc"
+	cp ./share/man/man1/tgftp.1 "$prefixDir/tgftp/share/man/man1"
+	cp ./share/man/man1/tgftp_log.1 "$prefixDir/tgftp/share/man/man1"
 
 elif [[ "$(basename $0)" == "uninstall.sh" ]]; then
 
+	#  remove a system installed tgftp
 	if [[ "$1" != "" ]]; then
 		rm -r "$prefixDir/tgftp"
+	#  remove a user installed tgftp
 	else
 		#  remove scripts and links from "$HOME/bin"
 		rm "$HOME/bin/tgftp"
